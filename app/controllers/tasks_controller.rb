dadb4680d6ee9f2a2385f51986fc721750a966e0
Ilia Colombini <ilia.colombini@gmail.com>
2020-11-10 15:07:21 +0100

    add a new task added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,21 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  private
+
+  # Strong parameters
+  def task_params
+    # Forbidden object 👇
+    params.require(:task).permit(:title, :details)
+  end
 end
