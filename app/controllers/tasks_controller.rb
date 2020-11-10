0251393188e822c6fde5bf8a33a8b6e06259d07d
Ilia Colombini <ilia.colombini@gmail.com>
2020-11-10 15:37:31 +0100

    edit task added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,11 +17,20 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
   private
 
   # Strong parameters
   def task_params
-    # Forbidden object 👇
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
